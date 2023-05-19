98e6373a8bac617a9d4696093019cece27f60438
NONHIGH <angelones_24@hotmail.com>
2023-05-19 19:37:00 -0300

    Create backend web store

---
+++ src/main/java/com/web_store/web_store/WebStoreApplication.java
@@ -0,0 +1,13 @@
+package com.web_store.web_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreApplication.class, args);
+	}
+
+}
